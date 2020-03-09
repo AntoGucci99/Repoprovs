ac85a56c4f135980a044153626102afe9789b710
giovanni.antonucci <giovanni.antonucci@beantech.it>
2020-03-09 14:24:57 +0100

    Aggiunta project

---
+++ provagit/provagit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace provagit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("questa è una prova git");
+        }
+    }
+}
